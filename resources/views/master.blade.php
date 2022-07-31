466e51d020b453d09404600575f59fe5daaa781a
Aminul Islam Joy <60224967+Joy602@users.noreply.github.com>
2022-07-31 20:38:23 +0600

    Cart Product listing done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -63,6 +63,15 @@ img.slider-img {
 .detail-img{
     height: 400px
 }
+.search-box{
+    width:500px !important
+}
+
+.cart-list-divider{
+    border-bottom: 1px solid #ccc;
+    margin-bottom: 20px;
+    padding-bottom: 20px;
+}
 
 </style>
 
