4fe1f5efdd244ab8d43812a87d5742cd0e22fd3a
Aminul Islam Joy <60224967+Joy602@users.noreply.github.com>
2022-07-31 21:39:02 +0600

    removing from cart done

--- resources/views/cartlist.blade.php
+++ resources/views/cartlist.blade.php
@@ -33,7 +33,7 @@
                 </div>
 
                 <div class="col-sm-3">
-                    <button class="btn btn-warning">Remove from cart</button>
+                    <a href="/removecart/{{$item->cart_id}}" class="btn btn-warning">Remove from cart</a>
                 </div>
             </div>
             @endforeach
